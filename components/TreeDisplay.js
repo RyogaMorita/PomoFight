7b71ca309a8836a5fe5fef70036d8f35a5a95a9d
森田凌雅 <macryou@moritaryoumiyabinoMacBook-Pro.local>
2026-04-05 12:12:07 +0900

    Fix tree images: use .jpg extension

--- components/TreeDisplay.js
+++ components/TreeDisplay.js
@@ -2,16 +2,16 @@ import { View, Text, Image, StyleSheet } from 'react-native'
 
 // 画像が揃ったら自動で使われる。なければ絵文字にフォールback
 const TREE_IMAGES = {
-  1: require('../assets/trees/tree_1.png'),
-  2: require('../assets/trees/tree_2.png'),
-  3: require('../assets/trees/tree_3.png'),
-  4: require('../assets/trees/tree_4.png'),
-  5: require('../assets/trees/tree_5.png'),
-  6: require('../assets/trees/tree_6.png'),
-  7: require('../assets/trees/tree_7.png'),
-  8: require('../assets/trees/tree_8.png'),
-  9: require('../assets/trees/tree_9.png'),
-  10: require('../assets/trees/tree_10.png'),
+  1: require('../assets/trees/tree_1.jpg'),
+  2: require('../assets/trees/tree_2.jpg'),
+  3: require('../assets/trees/tree_3.jpg'),
+  4: require('../assets/trees/tree_4.jpg'),
+  5: require('../assets/trees/tree_5.jpg'),
+  6: require('../assets/trees/tree_6.jpg'),
+  7: require('../assets/trees/tree_7.jpg'),
+  8: require('../assets/trees/tree_8.jpg'),
+  9: require('../assets/trees/tree_9.jpg'),
+  10: require('../assets/trees/tree_10.jpg'),
 }
 
 const TREE_EMOJIS = ['🌱', '🌱', '🌿', '🪴', '🌳', '🌳', '🌲', '🌲', '🎄', '🌴', '🎋']
